b6083a3ae4ac31c161adc51b75c16758f5d4571b
Michael Cummins <michaelcummins@Michaels-MacBook-Air.local>
2023-10-24 13:13:18 +0100

    Loaded mnist data and begun fedadmm exp

--- admm/models.py
+++ admm/models.py
@@ -1,6 +1,24 @@
 import torch
 import torch.nn as nn
 
+
+class mnistNet(nn.Module):
+
+    def __init__(self, in_channels, hidden1, out_channels, hidden2=None) -> None:
+        super().__init__()
+
+        layers = [nn.Linear(in_features=in_channels, out_features=hidden1), nn.ReLU()]
+        if hidden2 is not None:
+            layers.append(nn.Linear(in_features=hidden1, out_features=hidden2))
+            layers.append(nn.ReLU())
+            layers.append(nn.Linear(in_features=hidden2, out_features=out_channels))
+        else:
+            layers.append(nn.Linear(in_features=hidden1, out_features=out_channels))
+        layers.append(nn.Softmax())
+        self.net = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.net(x)
 class Dummy(nn.Module):
 
     def __init__(self, x) -> None:
